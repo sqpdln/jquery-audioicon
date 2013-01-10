73314be6056796eefb97de3fd06161f5d6fa87e9
sqpdln <thestoryends@gmail.com>
2013-01-10 21:57:56 +0100

    added property poly added minified version

--- README.md
+++ README.md
@@ -1,25 +1,35 @@
-jquery-audioicons
+
+=================
+jQuery-audioicons
+
 Open licence.
 =================
 
 This jQuery plugin is in development, but functional in it's simplicity.
 
-Make your DOM-elements sound on events!
+Make your DOM-elements trigger sound on events!
 
-Build with Web Audio API so requires Chrome or any other browser with support.
+Built with Web Audio API so requires Chrome or any other browser with support.
 Accepted file formats varies across browsers, .wav is generally always ok.
 
-read more here -> https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html
-
-
+=================
 Parameters so far:
+=================
 
 url: 'path/to/audiofile',
-trigger: 'eventtype',
-gain: value between 0-1 //optional
-
+trigger: 'eventtype', //optional, default click
+gain: value between 0-1 //optional, default 0.8
+poly: value between 1-20 //optional, default 4
 
 $(element).audioicon({
 	  key: 'value',
 	  key2: 'value2'
 });
+
+=================
+Funktionality
+=================
+
+The audioContext is attached to the window, and so the same one is used for all instances.
+
+More info -> https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html
--- audioicon.js
+++ audioicon.js
@@ -1,12 +1,25 @@
 (function( $ ) {
 	$.fn.audioicon = function(args) {
-		var el = this,
-			url = args.url,
-			trigger = args.trigger,
-			gain = args.gain,
-			audioContext,
-			props = {},
+				
+		var el = this,					//jquery object
+
+			//input arguments
+			trigger = args.trigger,		//event type
+			url = args.url,				//url
+			gain = args.gain,			//int 0-1
+			poly = args.poly,			//int 1-20, defaults to 4
+
+			//global vars
+			voices = [];				//the array to store source nodes
+			round = 0,					//count times played
+			el.buffer = null,				//properties
 			M = Math,
+
+			chainGain = function(node) {
+					node.connect(gainNode);
+					currentNode = gainNode;
+					return currentNode;
+			},
 
 			initialize = function() {
 				//init web audio context
@@ -15,17 +28,24 @@
 				}
 				else{
 					try {
-						audioContext = window.audioContext = new webkitAudioContext();
+						var audioContext = window.audioContext = new webkitAudioContext();
 					}
 					catch(e) {
 						alert( 'Web Audio API is not supported in this browser');
 						return;
 					}
 				}
-				
+
+
 				if (url		!== undefined)	{loadBuffer();}			else {return logg('No valid url provided.');}
-				if (trigger	!== undefined)	{el.bind(trigger,play);}else {return logg('No valid trigger provided.');}
-				if (gain	!== undefined)	{loadGain();}
+				if (trigger	!== undefined)	{el.bind(trigger,play);}else {el.bind('click',play);}
+				if (!isANumber(gain) || !(gain >= 0 && gain <= 1))	{gain = 0.8;}	loadGain();
+				if (!isANumber(poly) || !(poly > 0 && poly < 21))	{poly = 4;}		loadPoly(poly);
+
+			},
+
+			isANumber = function(nbr){
+				return typeof nbr == 'number' ? true : false;
 			},
 
 			//GET file and buffer it. $.ajax() would've been nice, but doesn't support arraybuffer
@@ -35,8 +55,8 @@
 				xhr.open('GET', url, true);
 				xhr.send();
 				xhr.onload = function() {
-					audioContext.decodeAudioData(xhr.response, function(buffer) {
-						props.buffer = buffer;
+					audioContext.decodeAudioData(xhr.response, function(b) {
+						el.buffer = b;
 						return;
 					});
 				};
@@ -47,14 +67,26 @@
 				gainNode.gain.value = (M.min(1, gain));
 			},
 
-			play = function() {
-				sourceNode = audioContext.createBufferSource();
-				sourceNode.buffer = props.buffer;
-				var currentNode = sourceNode;
-				if (gain) {
-					currentNode.connect(gainNode);
-					currentNode = gainNode;
+			loadPoly = function(nbrOfVoices) {
+				for (var i = 0; i < nbrOfVoices; i++) {
+					voices.push({node: undefined});//false means voice is not playing
+
 				}
+			},
+
+			play = function() {
+				var i = round++ % voices.length;
+				if (voices[i].node === undefined) {voices[i].gotNode = true;}
+				//polyphony limit reached, stop any eventual stream at node
+				else {sourceNode.stop(0);}
+
+				sourceNode = voices[i].node = audioContext.createBufferSource();
+				sourceNode.buffer = el.buffer;
+
+				//chaining
+				var currentNode = sourceNode;
+				if (gain) {currentNode = chainGain(currentNode);}
+
 				currentNode.connect(audioContext.destination);
 				sourceNode.start(0);
 			},
@@ -69,6 +101,5 @@
 		initialize();
 
 		return this;
-
 	};
 })( jQuery );
